d35d453a138b8e3aa332723f6889144b17c734d7
Hedlundaren <simhe813@student.liu.se>
2017-08-05 13:04:11 +0200

    heroku deploy

---
+++ release/config/index.js
@@ -0,0 +1,8 @@
+'use strict';
+var nconf = require('nconf');
+var configFile = __dirname + '/config.json';
+console.log('Using base configuration file ' + configFile);
+nconf.argv()
+    .env()
+    .file({ file: configFile });
+module.exports = nconf;
