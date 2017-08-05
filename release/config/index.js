b4ad8078271d4b61daa0dbb7e8309dce28825408
Hedlundaren <simhe813@student.liu.se>
2017-08-05 13:24:34 +0200

    restart

--- release/config/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var nconf = require('nconf');
-var configFile = __dirname + '/config.json';
-console.log('Using base configuration file ' + configFile);
-nconf.argv()
-    .env()
-    .file({ file: configFile });
-module.exports = nconf;
--- typings/tsd.d.ts
+++
@@ -1,6 +0,0 @@
-
-/// <reference path="express/express.d.ts" />
-/// <reference path="body-parser/body-parser.d.ts" />
-/// <reference path="errorhandler/errorhandler.d.ts" />
-/// <reference path="nconf/nconf.d.ts" />
-/// <reference path="node/node.d.ts" />
