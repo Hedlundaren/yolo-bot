d1013cb25a555d1b54232c9083023306e3413724
Hedlundaren <simhe813@student.liu.se>
2017-08-05 14:17:32 +0200

    Enabling functionality

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.get('/', function(req, res){
     res.send("Hi I am a chatbot")
 })
 
+token = "EAAbqignfEwoBAByuzb8lqhRlZCjqmKZBhr7FjSMDPcK9DZBU9xHAR1IXcZA47ehTG0t2849WRBjh1DTL026rNQ1rZCIDgavqC280vEtHrg1ZBq6dRgtDSQc8CIbwGMOAoZBBh90ZAZBmkyT3Y9lISFV1OT66t2fSZAlj1SGuZBAOhUSkAZDZD"
+
 // Facebook
 app.get('/webhook/', function(req, res){
     if(req.query['hub.verify_token'] === "yoloswag"){
@@ -21,6 +23,40 @@ app.get('/webhook/', function(req, res){
     }
     res.send("Wrong token")
 })
+
+
+app.post('/webhook/', function(){
+    let messaging_events = req.body.entry[0].messaging_events
+    for(let i = 0; i < messaging_events.length; i++){
+        let event = messaging_events[i]
+        let sender = event.sender.id
+        if(event.message && event.message.text){
+            let text = event.message.text
+            sendText(sender, "Text echo: " + text.substring(0, 100))
+        }
+    }
+    res.sendStatus(200)
+})
+
+function sendText(sender, text){
+    let messageData = {text: text}
+    request({
+        url: "https://yolo-bot.herokuapp.com/",
+        qs: {access_token, token},
+        method: "POST",
+        json: {
+            receipt: {id: sender},
+            message: messageData
+        }
+
+    }, function(error, response, body){
+        if(error){
+            console.log("sending error")
+        } else if(response.body.error){
+            console.log("response body error")
+        }
+    })
+}
 
 app.listen(app.get('port'), function(){
     console.log("running: port")
