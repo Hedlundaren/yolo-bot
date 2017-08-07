25c1412d280e89a3d0bd44352bac250f50ef2ee8
Hedlundaren <simhe813@student.liu.se>
2017-08-07 03:28:41 +0200

    cont

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/webhook/', function(req, res){
     for(let i = 0; i < messaging_events.length; i++){
         let event = messaging_events[i]
         let sender = event.sender.id
-        let sender_first_name = "Simon"
+        let sender_first_name = event.sender.name.substring(0, 100)
 
         let sender_info = getSenderInfo(sender)
 
