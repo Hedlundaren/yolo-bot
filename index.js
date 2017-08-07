10c2aeae67971486191838658521bd08971071fc
Hedlundaren <simhe813@student.liu.se>
2017-08-07 23:20:33 +0200

    testar require xhr

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const http = require('http')
 const app = express()
 
 
@@ -51,12 +50,10 @@ function getWeather(sender_id){
 
 
 // Get user info
-function getSenderInfo(sender){
+function getSenderInfo(sender_id){
 
-    let host = 'https://graph.facebook.com/'
-    let sender = '1375896805858790'
-    let token = '?access_token=EAAbqignfEwoBABzHXXS2GtsYwdf1vKXVg7Rbin0ycwg7N4eAsSllEeBETKsH50KjtZBWAXjvpmXhM9dOCkSMxsHTYM8V6cuKoPgQIOQDqsnXijfivqWejpP9wDskcZBZBeIsEX8B9vpvaFe1wtadWLhzM9kXTCpkarYPUe74QZDZD'
-    let url = host + sender + token
+    let path = 'https://graph.facebook.com/' + sender_id + '?access_token=' + token
+
 }
 
 // Handling incoming messages
