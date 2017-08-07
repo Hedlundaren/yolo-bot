83fb19c67c369b36192a2e981becfb0e09e7d64a
Hedlundaren <simhe813@student.liu.se>
2017-08-07 23:16:05 +0200

    testar require xhr

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const app = express()
 const http = require('http')
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
+
+const app = express()
+
 
 app.set('port', (process.env.PORT || 5000))
 
