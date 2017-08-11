ba2d4fcd4b6a062389057a2bdf68bc52dff9bf78
Hedlundaren <simhe813@student.liu.se>
2017-08-11 12:30:20 +0200

    vgd

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function eniro(sender, search) {
         .then(({data}) => {
             text += 'Totalt: ' + data.adverts.length.toString() + 'st\n'
             for (let i = 0; i < data.adverts.length; i++) {
-                text += (i + 1) + '. 🌈 \n' + data.adverts[i].companyInfo.companyName + '\n'
+                text += (i + 1) + '. 🌈🏠 \n' + data.adverts[i].companyInfo.companyName + '\n'
                 text += data.adverts[i].address.streetName + '\n'
                 if(data.adverts[i].phoneNumbers.length > 0)
                     text += data.adverts[i].phoneNumbers[0].phoneNumber + '\n'
